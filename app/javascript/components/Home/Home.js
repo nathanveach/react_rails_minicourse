5c31ac4e04726e9c582e7fa56c56b39a3532944b
nathanveach <nathanbveach@gmail.com>
2020-05-06 17:34:25 -0700

    Add react and bootstrap

---
+++ app/javascript/components/Home/Home.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import Jumbotron from './Jumbotron'
+
+class Home extends Component{
+	constructor(){
+		super()
+
+		this.state = {
+			course_modules: [
+				{ id: 1, title: '1. Setting up a new Ruby on Rails App with React.', description: 'lorem ipsum', active: false },
+				{ id: 2, title: '2. Adding React to an Existing Rails App.', description: 'lorem ipsum', active: false },
+				{ id: 3, title: '3. Building a Hello World App.', description: 'lorem ipsum', active: false },
+				{ id: 4, title: '4. Adding React Router Dom to your App.', description: 'lorem ipsum', active: false },
+			]
+		}
+	}
+
+	render() {
+		return(
+			<div>
+				<Jumbotron/>
+			</div>
+			)
+	}
+}
+
+export default Home
