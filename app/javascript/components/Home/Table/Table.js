34ca91fe197dbd0377c04f35c72a13f6132269a8
nathanveach <nathanbveach@gmail.com>
2020-05-08 21:44:45 -0700

    Add table and thumbnail components to home page

---
+++ app/javascript/components/Home/Table/Table.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import Item from './Item'
+
+class Table extends Component {
+	constructor(props){
+		super(props)
+	}
+
+	render(){
+		const items = this.props.course_modules.map( (data) => {
+			return <Item key={data.id} title={data.title} description={data.description}/>
+		})
+
+		return(
+			<div className="py-5">
+				<div className="container">
+					<div className="text-center">
+						<h2 className="py-4">React for Rails Developers - Videos</h2>
+					</div>
+
+					{items}
+				</div>
+			</div>
+		)
+	}
+}
+
+
+export default Table
